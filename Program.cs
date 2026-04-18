bc1818f612ac80fd1a5c8bdabd7db37813dbd898
Emanuel Chiaburu <emanchiaburu@hotmail.com>
2026-04-18 10:22:12 -0400

    Initial commit of BlazorTaskScheduler project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using BlazorTaskScheduler.Components;
+using BlazorTaskScheduler.QuartzJobs;
+using Quartz;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddQuartz(static q =>
+{
+    // Add a test job
+    q.AddJob<SampleJob>(opts => opts.WithIdentity("SampleJob"));
+    // Trigger job every 10 seconds
+    q.AddTrigger(opts => opts
+        .ForJob("SampleJob")
+        .WithIdentity("SampleJob-trigger")
+        .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever()));
+});
+
+// Start Quartz as a background service
+builder.Services.AddQuartzHostedService();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ QuartzJobs/SampleJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace BlazorTaskScheduler.QuartzJobs
+{
+    public class SampleJob : IJob
+    {
+        private readonly ILogger<SampleJob> _logger;
+
+        public SampleJob(ILogger<SampleJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Sample job executed at {time}",DateTime.Now);
+            return Task.CompletedTask;
+        }
+    }
+}
